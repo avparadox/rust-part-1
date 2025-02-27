69637d46ae0be9f292c6754b5822d32b5570b363
avparadox <avyas8927@gmail.com>
2025-02-27 17:42:10 +0530

    add: mutability & loops

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 fn main() {
     println!("Hello, Aditya!");
 
-    let x: i8 = -4;
-    let x1 = -4;
-    let y: u8 = 101;
-    // by default types of +ve ints are given i32. 
-    // if it is a positive number and u know it surely, make it u8 or whaterver.
-    let y2 = 101;
-    let z: f32 = -95.5;
+    // let x: i8 = -4;
+    // let x1 = -4;
+    // let y: u8 = 101;
+    // // by default types of +ve ints are given i32. 
+    // // if it is a positive number and u know it surely, make it u8 or whaterver.
+    // let y2 = 101;
+    // let z: f32 = -95.5;
 
-    println!("x: {},",x);
-    println!("x1: {},",x1);
-    println!("y: {},",y);
-    println!("y2: {},",y2);
-    println!("z: {},",z);
+    // println!("x: {},",x);
+    // println!("x1: {},",x1);
+    // println!("y: {},",y);
+    // println!("y2: {},",y2);
+    // println!("z: {},",z);
+
+    // Muts & Loops
+
+    let mut x: i8= 0;
+    
+    for _i in 0..1000 {
+       x =  x +  100;
+    }
+
+    println!("x: {}", x)
+
+
+
+
 }
