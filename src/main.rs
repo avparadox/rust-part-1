79679e938bb59732d98c8ed1ce8c05e074f23a88
avparadox <avyas8927@gmail.com>
2025-02-27 17:16:04 +0530

    add: about signed & unsigned ints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, Aditya!");
+
+    let x: i8 = -4;
+    let x1 = -4;
+    let y: u8 = 101;
+    // by default types of +ve ints are given i32. 
+    // if it is a positive number and u know it surely, make it u8 or whaterver.
+    let y2 = 101;
+    let z: f32 = -95.5;
+
+    println!("x: {},",x);
+    println!("x1: {},",x1);
+    println!("y: {},",y);
+    println!("y2: {},",y2);
+    println!("z: {},",z);
 }
