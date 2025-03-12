2f062ed9fbc67cedcbddb132f9f59f5fa2306bbd
avparadox <avyas8927@gmail.com>
2025-03-12 16:27:55 +0530

    cloning on heap memory

--- src/main.rs
+++ src/main.rs
@@ -106,42 +106,50 @@ fn main() {
     // }
 
     // Stack & Heap Chapter
-        stack_fn();   // Call the function that uses stack memory
-        heap_fn();    // Call the function that uses heap memory
-        update_string();  // Call the function that changes size of variable at runtime
-
-}
-
-
-fn stack_fn() {
-    // Declare a few integers on the stack
-    let a = 10;
-    let b = 20;
-    let c = a + b;
-    println!("Stack function: The sum of {} and {} is {}", a, b, c);
-}
-
-fn heap_fn() {
-    // Create a string, which is allocated on the heap
-    let s1 = String::from("Hello");
-    let s2 = String::from("World");
-    let combined = format!("{} {}", s1, s2);
-    println!("Heap function: Combined string is '{}'", combined);
-}
-
-fn update_string() {
-    // Start with a base string on the heap
-    let mut s = String::from("Initial string");
-    println!("Before update: {}", s);
-    println!("Capacity: {}, Length: {}, Pointer: {:p}", s.capacity(), s.len(), s.as_ptr());
+        // stack_fn();   // Call the function that uses stack memory
+        // heap_fn();    // Call the function that uses heap memory
+        // update_string();  // Call the function that changes size of variable at runtime
     
-    // Append some text to the string
-   for _ in 0..100{
-    s.push_str(" and some additional text \n");
-    // println!("After update: {}", s);
-    println!("Capacity: {}, Length: {}, Pointer: {:p}", s.capacity(), s.len(), s.as_ptr());
-   }
+    // Ownerships - Clones
+
+    let s1 = String::from("Hello");
+    let s2 = s1.clone();
+    println!("{} S1, {:p}", s1, s1.as_ptr());
+    println!("{} S2, {:p}", s2, s2.as_ptr());
+
+
+
 }
+
+// fn stack_fn() {
+//     // Declare a few integers on the stack
+//     let a = 10;
+//     let b = 20;
+//     let c = a + b;
+//     println!("Stack function: The sum of {} and {} is {}", a, b, c);
+// }
+
+// fn heap_fn() {
+//     // Create a string, which is allocated on the heap
+//     let s1 = String::from("Hello");
+//     let s2 = String::from("World");
+//     let combined = format!("{} {}", s1, s2);
+//     println!("Heap function: Combined string is '{}'", combined);
+// }
+
+// fn update_string() {
+//     // Start with a base string on the heap
+//     let mut s = String::from("Initial string");
+//     println!("Before update: {}", s);
+//     println!("Capacity: {}, Length: {}, Pointer: {:p}", s.capacity(), s.len(), s.as_ptr());
+    
+//     // Append some text to the string
+//    for _ in 0..100{
+//     s.push_str(" and some additional text \n");
+//     // println!("After update: {}", s);
+//     println!("Capacity: {}, Length: {}, Pointer: {:p}", s.capacity(), s.len(), s.as_ptr());
+//    }
+// }
 
 
 // Functions 
